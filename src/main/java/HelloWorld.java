cb6e9d30f5fcef703820722706b29216ff7cd342
XJ20116921 <40447024+XJ20116921@users.noreply.github.com>
2018-08-06 17:30:39 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,9 @@ public class HelloWorld {
 
     public static  void main(String[] args){
 
-        System.out.println("我们都是好孩子 呵呵呵呵");
-        System.out.println("Hello Git 呵呵呵呵好");
+        System.out.println("我们都是好孩子 呀呀呀");
+        System.out.println("Hello Git 呀呀呀");
+
 
     }
 }
