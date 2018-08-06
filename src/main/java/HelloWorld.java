3d15a18fb37b017e55cb666f8a9efdb41fdc41c6
XJ20116921 <40447024+XJ20116921@users.noreply.github.com>
2018-08-06 17:30:39 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static  void main(String[] args){
 
         System.out.println("我们都是好孩子 呀呀呀");
-        System.out.println("Hello Git 呀呀呀");
+        System.out.println("Hello Git");
 
 
     }
