5dd2ebb5dd119d8beddabd697d1e9b8bc6c2b91c
xujian <xujian04637@hellobike.com>
2018-08-06 17:19:44 +0800

    base

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
 
     public static  void main(String[] args){
 
-        System.out.println("我们都是好孩子哈哈哈");
-        System.out.println("Hello Git 呀呀呀");
+        System.out.println("我们都是好孩子");
+        System.out.println("Hello Git");
 
     }
 }
